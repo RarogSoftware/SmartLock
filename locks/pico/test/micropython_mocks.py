21b8f3ebcaeacd4e71f92ff190756c9271f736e6
Paweł Przytarski <pawelprzytarski@hotmail.com>
2023-09-21 20:07:20 +0200

    SL-3: Make tests to run on Pico and PC

--- locks/pico/test/micropython_mocks.py
+++ locks/pico/test/micropython_mocks.py
@@ -1,10 +1,32 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) 2023, Pawel Przytarski                                -
+#                                                                              -
+#   Licensed under the Apache License, Version 2.0 (the "License");            -
+#   you may not use this file except in compliance with the License.           -
+#   You may obtain a copy of the License at                                    -
+#   http://www.apache.org/licenses/LICENSE-2.0                                 -
+# ------------------------------------------------------------------------------
+import sys
+
 def get_micropython_mocks():
-    from mock import MagicMock
+    from unittest.mock import MagicMock
+    import test.mock.micropython.micropython as micropython
+    import test.mock.micropython.machine as machine
+    import test.mock.micropython.aioble as aioble
+
     return dict({
-        "micropython": MagicMock(),
+        "micropython": micropython,
         "collections": {
             "abc": MagicMock()
         },
-        "aioble": MagicMock(),
-        "bluetooth": MagicMock()
+        "aioble": aioble,
+        "bluetooth": MagicMock(),
+        "machine": machine
     })
+
+
+if "micropython" != sys.implementation.name:
+    print("Mocking micropython libraries")
+    mocks = get_micropython_mocks()
+    for key in mocks:
+        sys.modules[key] = mocks[key]
--- locks/pico/test/mock/micropython/machine.py
+++ locks/pico/test/mock/micropython/machine.py
@@ -1,17 +1,21 @@
 # ------------------------------------------------------------------------------
-#  Copyright (c) 2023 onwards, Pawel Przytarski                                -
+#  Copyright (c) 2023, Pawel Przytarski                                        -
 #                                                                              -
 #   Licensed under the Apache License, Version 2.0 (the "License");            -
 #   you may not use this file except in compliance with the License.           -
 #   You may obtain a copy of the License at                                    -
 #   http://www.apache.org/licenses/LICENSE-2.0                                 -
 # ------------------------------------------------------------------------------
+import time
 
-import sys
 
-from .test_runner import config
+class RTC:
+    def __init__(self):
+        self._offset = 0
 
-if sys.implementation.name == 'micropython':
-    from .test_runner import run_tests
-elif sys.implementation.name == "cpython":
-    import test.micropython_mocks
+    def datetime(self, datetime=None):
+        if datetime is not None:
+            (year, month, day, weekday, hour, minute, second, *_) = datetime
+            unix = time.mktime((year, month, day, hour, minute, second, weekday, -1, 0))
+            self._offset = time.mktime(time.localtime()) - unix
+        return time.localtime(time.mktime(time.localtime()) - self._offset)
